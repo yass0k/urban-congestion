d9123a912bd2a594758dcf84fa8e9a8654b37bc2
Yasmeen <yasmeenkhattab@outlook.com>
2025-10-22 09:27:17 +0300

    Updated website content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <div className="page">
       <h1>Contact Us</h1>
-      <p>Only if you are a 6'4 male with big muscles and alot of money</p>
+      <p>If you have any further questions, please feel free to contact us!</p>
     </div>
   )
 }
