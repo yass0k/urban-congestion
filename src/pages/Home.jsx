78ed2adc918031a9cd7daf48c33087ccfcfcab4c
Yasmeen <yasmeenkhattab@outlook.com>
2025-10-22 10:38:06 +0300

    Trigger redeploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import FlipCard from "../components/FlipCard";
+import "./Home.css";
 
 function Home() {
   const navigate = useNavigate();
@@ -7,48 +8,33 @@ function Home() {
   return (
     <div className="page">
       <h1>Urban Congestion Smart Monitor</h1>
-      <p>Track real-time traffic and make your journey smoother 🤍 </p>
-      <div style={{ marginTop: "2rem" }}>
-        <button
-          className="cta-btn"
-          style={{ marginRight: "1rem" }}
-          onClick={() => navigate("/dashboard")}
-        >
+      <p>Track real-time traffic and make your journey smoother 🤍</p>
+
+      <div>
+        <button className="cta-btn" onClick={() => navigate("/dashboard")}>
           Go to Dashboard
         </button>
-        <button
-          className="cta-btn"
-          onClick={() => navigate("/about")}
-        >
+        <button className="cta-btn" onClick={() => navigate("/about")}>
           Learn More
         </button>
       </div>
-          <div
-      style={{
-        marginTop: "3rem",
-        display: "flex",
-        justifyContent: "center",
-        gap: "2rem",
-        flexWrap: "wrap",
-      }}
-    >
-      <FlipCard
-        title="What is our purpose?"
-        content="To alleviate urban congestion through real-time monitoring and smart solutions."
-      />
 
-      <FlipCard
-        title="How does it work?"
-        content="Sensors collect live traffic data that’s visualized on our dashboard map."
-      />
-
-      <FlipCard
-        title="Why Egypt?"
-        content="The project is built in Egypt to address local traffic challenges and improve daily commutes."
-      />
+      <div className="cards-container">
+        <FlipCard
+          title="What is our purpose?"
+          content="To alleviate urban congestion through real-time monitoring and smart solutions."
+        />
+        <FlipCard
+          title="How does it work?"
+          content="Sensors collect live traffic data that’s visualized on our dashboard map."
+        />
+        <FlipCard
+          title="Why Egypt?"
+          content="The project is built in Egypt to address local traffic challenges and improve daily commutes."
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
